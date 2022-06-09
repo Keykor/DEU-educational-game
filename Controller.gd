c98bfa4b77483bdc36630c6b8c7b19fc8050d1a9
unknown <leonardogermanlozabonora@gmail.com>
2022-06-09 20:54:34 -0300

    feat: scene changer

--- Controller.gd
+++ Controller.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
+var active_scene
+
 func _ready():
-	get_node("Timer").start()
+	$Timer.start()
+	active_scene = $Start
 	pass
 
 func _process(delta):
@@ -9,4 +12,11 @@ func _process(delta):
 
 func _on_Timer_timeout():
 	print("Se termino el juego bro")
+	pass
+
+func _on_change_scene(scene_name):
+	var next_scene = load("res://Scenes/" + scene_name + ".tscn").instance()
+	add_child(next_scene)
+	active_scene.queue_free()
+	active_scene = next_scene
 	pass
