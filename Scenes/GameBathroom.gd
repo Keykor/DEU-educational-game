4f5edfb1acec07cbed5ddbab2015d5e04b1b2f29
Felipe Dioguardi <felipedioguardi@gmail.com>
2022-06-13 00:07:55 -0300

    feat: add buttons to change between scenes
    Co-authored-by: Julian Marques de Abrantes <marjulian99@gmail.com>

---
+++ Scenes/GameBathroom.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+signal change_scene(scene_name)
+
+func _ready():
+	pass 
+
+func _on_button_pressed(scene_name):
+	print(scene_name)
+	#EMITIR UNA SEÑAL
+	emit_signal("change_scene", scene_name)
+	pass
